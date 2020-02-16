fd92dd52488826e0650acc0c23deadb967b746e5
Irina Yavors <irinayavors@gmail.com>
2020-02-16 23:56:13 +0200

    Added first version whit checks

---
+++ predict.py
@@ -0,0 +1,26 @@
+import random
+
+PREDICTION_1 = "Лапа"
+PREDICTION_2 = "Клюв"
+PREDICTION_3= "Крылышко"
+PREDICTION_4 = "Перья"
+
+prediction_list = [PREDICTION_1, PREDICTION_2, PREDICTION_3, PREDICTION_4]
+prediction_list_len = len(prediction_list)
+
+def make_prediction(number):
+    number = int(number)
+
+    random.shuffle(prediction_list)
+    prediction = prediction_list[number-1]
+    print(prediction)
+
+while True:
+    pred_number = input(f"Введите число от 1 до {prediction_list_len} \n")
+    if not pred_number.isdigit():
+        print("Введите число, не расстраивайте птичку киви")
+    elif not 0 < int(pred_number) <= prediction_list_len:
+        print(f"Вы снова расстроили птичку киви. Введенное число не входит в интервал от 1 до {prediction_list_len}")
+    else:
+        make_prediction(pred_number)
+        break
