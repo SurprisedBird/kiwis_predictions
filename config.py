eb3c3738e297a5a220861e144043d4ccc9ee98cd
Irina Yavors <irinayavors@gmail.com>
2020-03-03 15:27:35 +0200

    Created bot (whithout webhook)

---
+++ config.py
@@ -0,0 +1,9 @@
+TOKEN = '983745915:AAGgFoi-i7BB_tINp3jLs9BzFt68xA6wfcE'
+MAIN_URL = f'http://api.telegram.org/bot{TOKEN}'
+
+PREDICTION_1 = "Лапа"
+PREDICTION_2 = "Клюв"
+PREDICTION_3= "Крылышко"
+PREDICTION_4 = "Перья"
+
+prediction_list = [PREDICTION_1, PREDICTION_2, PREDICTION_3, PREDICTION_4]
