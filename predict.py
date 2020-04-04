0d4393be4c0d61c57e9c1b59607e95c465d110ea
Irina Yavors <irinayavors@gmail.com>
2020-04-04 18:15:11 +0300

    Deleted useless decorator. Removed some useless comments

--- predict.py
+++ predict.py
@@ -1,24 +1,9 @@
 import random
 import re
-
-def is_in_range(func):
-    def wrapper(*args):
-        number = func(*args)
-        if not 0 < int(number) <= args[0].prediction_list_len:
-            message_text = f"Птички киви живут до 60-ти лет. И она навсегда запомнит, что введенное вами число не входит в интервал от 1 до {args[0].prediction_list_len}"
-        else:
-            message_text = int(number)
-        return message_text
-    return wrapper
+import config
 
 class Predictions():
-
-    PREDICTION_1 = "Лапа"
-    PREDICTION_2 = "Клюв"
-    PREDICTION_3= "Крылышко"
-    PREDICTION_4 = "Перья"
-
-    prediction_list = [PREDICTION_1, PREDICTION_2, PREDICTION_3, PREDICTION_4]
+    prediction_list = config.prediction_list
     prediction_list_len = len(prediction_list)
 
     def shuffle_predictions(self):
@@ -30,31 +15,20 @@ class Predictions():
 
         return text
 
-    @is_in_range
     def convert_to_int(self, number):
         print(number)
         number = int(number)
         return number
 
     def process_input(self, pred_number):
-        text = pred_number
         is_any_number = any(i.isdigit() for i in pred_number)
         if is_any_number:
             text = re.search(r'\d+', pred_number)[0]
-            text = self.convert_to_int(text)
+            if not 0 < int(text) <= self.prediction_list_len:
+                text = f"Птички киви живут до 60-ти лет. И она навсегда запомнит, что введенное вами число не входит в интервал от 1 до {self.prediction_list_len}"
+            else:
+                text = int(text)
         else:
             text = "Введите число, не расстраивайте птичку киви"
         
         return text
-
-
-        
-"""     def check_strint(self, pred_number):
-        if not pred_number.isdigit():
-            text = "Введите число, не расстраивайте птичку киви"
-            is_any_number = any(i.isdigit() for i in pred_number)
-            if is_any_number:
-                number = re.search(r'\d+', pred_number)[0]
-                number = int(text) """
-                
-        
